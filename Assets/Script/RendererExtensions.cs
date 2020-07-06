9b225969285ea0aadf4ff7caefcb160e8d44e332
HamedAjaj <57003501+HamedAjaj@users.noreply.github.com>
2020-07-06 20:00:51 +0200

    Our game

---
+++ Assets/Script/RendererExtensions.cs
@@ -0,0 +1,12 @@
+﻿//  This C# class gives simple extension access to checking if an Renderer is rendered by a specific Camera. //
+
+using UnityEngine;
+
+public static class RendererExtensions
+{
+	public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+	{
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+		return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+	}
+}
